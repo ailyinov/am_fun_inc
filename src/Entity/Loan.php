0c16d8bd68afe0482284eec78e85fa14f580d687
Andrei Ilinov <ilynov.andrey@gmail.com>
2024-06-07 15:06:16 +0800

    Add loan entity

---
+++ src/Entity/Loan.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LoanRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LoanRepository::class)]
+class Loan
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\Column]
+    private int $term_days;
+
+    #[ORM\Column]
+    private int $percent;
+
+    #[ORM\Column]
+    private int $amount;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTermDays(): int
+    {
+        return $this->term_days;
+    }
+
+    public function setTermDays(int $term_days): static
+    {
+        $this->term_days = $term_days;
+
+        return $this;
+    }
+
+    public function getPercent(): int
+    {
+        return $this->percent;
+    }
+
+    public function setPercent(int $percent): static
+    {
+        $this->percent = $percent;
+
+        return $this;
+    }
+
+    public function getAmount(): iint
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+}
